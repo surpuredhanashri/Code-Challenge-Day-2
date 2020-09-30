781e789db2b730f7a1b8c53ffd5419eeba0aff8f
surpuredhanashri <surpuredhanashri@gmail.com>
2020-10-01 02:43:05 +0530

    DONE

--- CALCULATING SP/src/index.js
+++ CALCULATING SP/src/index.js
@@ -1,9 +1,16 @@
-//A man buys a cycle for Rs. a and sells it at a loss of b%. What is the selling price of the cycle?
+//A man buys a cycle for Rs. a and sells it at a loss of b%. 
+// What is the selling price of the cycle?
 // Write a Function to compute the roundof selling price.
 
-// Write a Function to return compute the doundof selling price.
+// Write a Function to return compute the doundof selling 
+// price.
+
 function find_SellingPrice(a,b){
-    
-    return null;
+    let sell = 100-b;
+    let sp = Math.round((sell/100)*a);
+    return sp;
 }
+
+find_SellingPrice(a,b);
+
 //Use SpecRunner to check the Test Cases.
--- THREE IDIOTS/src/index.js
+++ THREE IDIOTS/src/index.js
@@ -7,17 +7,19 @@
 // Binoy told Dinesh that his house is at the midpoint of the line joining Ajay's house and Chandru's house. 
 // Dinesh was puzzled. Can you help Dinesh out?
 
-//Given the coordinates of the 2 end points of a line (x1,y1) and (x2,y2), write a  function to find the midpoint of the line.
+//Given the coordinates of the 2 end points of a line (x1,y1) and (x2,y2), write a  
+// function to find the midpoint of the line.
 
 // Write the function to return the midpoint M1 of the line.
 function find_MidPoint_M1(X1,Y1,X2,Y2){
-
+   M1 = ((X1+X2)/2);
     return M1;
+    
 }
 
 // Write the function to return the midpoint M2 of the line.
 function find_MidPoint_M2(X1,Y1,X2,Y2){
-    
+    M2 = ((Y1+Y2)/2);
     return M2;
 }
 
